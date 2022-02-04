801ad0f93e11b33aea6400b16a55ddb5dce5c938
Kat <51405063+ArchiGen@users.noreply.github.com>
2022-02-04 15:59:34 +0100

    Inital commit

---
+++ src/Dictionary.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Dictionary() {
+  return (
+    <div className="Container">
+      <h1>Dictionary App</h1>
+      <button type="button" className="btn btn-primary">
+        hello dictionary
+      </button>
+    </div>
+  );
+}
