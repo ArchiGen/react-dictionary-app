28374c23acc138185aaa30941d52447f69931e8a
Kat <51405063+ArchiGen@users.noreply.github.com>
2022-02-05 13:44:31 +0100

    Search Engine added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(keyword);
+  }
+
+  function handleChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="Container">
       <h1>Dictionary App</h1>
-      <button type="button" className="btn btn-primary">
-        hello dictionary
-      </button>
+
+      <form className="form" onSubmit={search}>
+        <input type="search" placeholder="word" onChange={handleChange}></input>
+        <button type="submit" className="btn btn-primary">
+          search
+        </button>
+      </form>
     </div>
   );
 }
